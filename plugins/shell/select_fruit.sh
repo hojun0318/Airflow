5e7533a8980fa3773da8fbadf0813a9e93050ea7
roa_0318 <roa_0318@naver.com>
2024-05-25 04:41:38 +0900

    add plugins

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+        echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ];then
+        echo "You selected Orange!"
+if [ $FRUIT == GRAPE ];then
+        echo "You selected Grape!"
+else
+        echo "You selected other Fruit!"
+fi
