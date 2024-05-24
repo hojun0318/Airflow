ff1a70450e0e3b323cce60b0c2cd7306a90e0907
roa_0318 <roa_0318@naver.com>
2024-05-25 05:26:05 +0900

    Operrating plugins

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1", 
-    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     
@@ -21,4 +21,3 @@ with DAG(
     )
 
     t1_orange >> t2_avocado
-
